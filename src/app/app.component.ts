1241dbe691060bbb1150b091cb57e951ef9e30eb
Richi1991 <ricardo.egea2@gmail.com>
2026-02-22 02:10:03 +0100

    añadidas metricas de usuario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics, logEvent } from 'firebase/analytics';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { environment } from '../environments/environment';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   imports: [IonApp, IonRouterOutlet],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor() {}
+
+   ngOnInit() {
+    const app       = initializeApp(environment.firebase);
+    const analytics = getAnalytics(app);
+    logEvent(analytics, 'app_open');
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,17 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey:            "AIzaSyDO_kwNsftH62q5vmWWk1hwxeA_T0v-91Q",
+    authDomain:        "embalses-51bdd.firebaseapp.com",
+    projectId:         "embalses-51bdd",
+    storageBucket:     "embalses-51bdd.firebasestorage.app",
+    messagingSenderId: "56742035827",
+    appId:             "1:56742035827:web:cc5fc8a3c8e213f28fc506",
+    measurementId:     "G-ZZ2EL16G0F"
+  }
 };
 
 /*
