1f036d69dd8e8c3041b73498a537ec1b480bb229
Richi1991 <ricardo.egea2@gmail.com>
2026-02-24 13:04:03 +0100

    withHashLocation para que cargue primero el index.html

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http'; // IMPORTANTE
 import { AppComponent } from './app/app.component';
 import { provideIonicAngular } from '@ionic/angular/standalone';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideIonicAngular(),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideHttpClient(), // ESTO PERMITE QUE EL SERVICIO HABLE CON JAVA
   ],
 });
